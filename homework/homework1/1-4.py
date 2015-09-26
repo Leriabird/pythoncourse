f3ee4725c0dd8292ab6bbb480eecf2d0d33c50a2
Leriabird <leriapingui@gmail.com>
2015-09-26 14:58:58 +0300

    /

--- homework/homework1/1-4.py
+++ homework/homework1/1-4.py
@@ -1,9 +1,9 @@
 data = (input().split(' '))
-spisok = [int(i) for i in data]
-spisok1 = spisok[0:len(spisok):2]
-spisok1.sort()
-spisok2 = spisok[1:len(spisok):2]
-spisok2.sort(reverse=True)
+lst = [int(i) for i in data]
+lst1 = lst[0:len(lst):2]
+lst1.sort()
+lst2 = spisok[1:len(lst):2]
+lst2.sort(reverse=True)
 fin = []
-for i in range(len(spisok1)):
-    print(spisok1[i], spisok2[i], end = ' ')
+for i in range(len(lst1)):
+    print(lst1[i], lst2[i], end = ' ')
