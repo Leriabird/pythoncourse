fee3a6d96c2e448a12342d6e9c497f9bc2a1bee9
Leriabird <leriapingui@gmail.com>
2015-12-05 13:02:18 +0300

    t2
    t2

--- exam2/task2.py
+++ exam2/task2.py
@@ -1,1 +1,18 @@
 __author__ = 'Leria'
+
+import re
+import urllib
+import requests
+
+with open('links.txt', 'r') as l:
+    lin = l.read()
+    lst = lin.split('\n')
+    mailset = []
+    for char in lst:
+        text = requests.get(char).text
+        r = "[a-z0-9\.\-+_]+@[a-z0-9\.\-+_]+\.[a-z]+"
+        temp = re.findall(r, text)
+        tempout = list(set(temp))
+        mailset.extend(tempout)
+    for char in mailset:
+        print(char)
--- exam2/task3.py
+++ exam2/task3.py
@@ -5,17 +5,17 @@ import urllib2
 import requests
 import lxml
 
-url = "http://string-db.org/api/psi-mi/interactions?identifier=APP&required_score=800&limit=5"
-s = urllib2.urlopen(url)
-contents = s.read()
-
-contents = contents.split("\n")
-contents[1] = "<entrySet level=\"2\" version=\"5\">"
-contents = "\n".join(contents)
-
-file = open("first_hundred_string_app1.xml", 'w')
-file.write(contents)
-file.close()
-tree = ET.parse('first_hundred_string_app1.xml')
-root = tree.getroot()
-print(root)
+# url = "http://string-db.org/api/psi-mi/interactions?identifier=APP&required_score=800&limit=5"
+# s = urllib2.urlopen(url)
+# contents = s.read()
+#
+# contents = contents.split("\n")
+# contents[1] = "<entrySet level=\"2\" version=\"5\">"
+# contents = "\n".join(contents)
+#
+# file = open("first_hundred_string_app1.xml", 'w')
+# file.write(contents)
+# file.close()
+# tree = ET.parse('first_hundred_string_app1.xml')
+# root = tree.getroot()
+# print(root)
